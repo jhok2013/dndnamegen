76e0fe2b6f9293f238f380c0095bddba187d1cba
jhok2013 <jhok2013@gmail.com>
2020-10-20 23:21:13 -0600

    Added tests for chromedriver.exe paths. Updated the generate_names function.

--- tests/test_name_gen_api.py
+++ tests/test_name_gen_api.py
@@ -10,11 +10,11 @@ from dndnamegen.NameGenAPI import NameGenAPI #type: ignore
 
 class test_dndnamegen(unittest.TestCase):
     '''
-
+    Handles all the tests for the NameGenAPI.
     '''
     wsl_path: Path = Path('../bin/chromedriver-linux64')
-    #win_path: str = "C:\Program Files (x86)\chromedriver.exe"
-    win_path: str = r"C:\projects\dndnamegen\bin\chromedriver.exe"
+    win_path: str = "C:\Program Files (x86)\chromedriver.exe"
+    win_bin_path: str = "..\\bin\chromedriver.exe"
     
     def setUp(self):
         return super().setUp()
@@ -22,12 +22,31 @@ class test_dndnamegen(unittest.TestCase):
     def tearDown(self):
         return super().tearDown()
     
-    def test_get_names(self) -> None:
+    def test_win_path(self) -> None:
         '''
-
+        Tests whether the NameGenAPI can run with the default
+        location of the chromedriver.exe on windows.
         '''
-        name_gen_api: NameGenAPI = NameGenAPI(binary_path=self.win_path)
-        name_gen_api.generate_names()
+        try:
+            name_gen_api: NameGenAPI = NameGenAPI(binary_path=self.win_path)
+            name_gen_api.generate_names()
+            self.assertTrue(True, "Test: test_win_path has passed.")
+        except Exception as e:
+            print(e)
+            self.fail("Failure: test_win_path has failed.")
+    
+    def test_win_bin(self) -> None:
+        '''
+        Tests whether the chromedriver.exe runs can run in the
+        built-in bin folder of the program.
+        '''
+        try:
+            name_gen_api: NameGenAPI = NameGenAPI(binary_path=self.win_bin_path)
+            name_gen_api.generate_names()
+            self.assertTrue(True, "Test: test_win_bin has passed.")
+        except Exception as e:
+            print(e)
+            self.fail("Failure: test_win_bin has failed.")
 
 if __name__ == "__main__":
     unittest.main()
